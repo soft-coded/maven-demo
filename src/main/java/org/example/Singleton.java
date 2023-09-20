cb00a1bd478c5224f29c2e232388b428522a693e
shrutanten <shrutanten.work@gmail.com>
2023-09-20 14:50:24 +0530

    Format the code better

--- src/main/java/org/example/Singleton.java
+++ src/main/java/org/example/Singleton.java
@@ -2,23 +2,23 @@ package org.example;
 
 public class Singleton {
 
-     private static  Singleton singleton = new Singleton();
+    private static Singleton singleton = new Singleton();
 
-    private Singleton() {}
-
+    private Singleton() {
+    }
 
     public static Singleton getSingleton() {
-         return singleton;
+        return singleton;
     }
 }
 
 class Test {
     public static void main(String[] args) {
-       Singleton st1  = Singleton.getSingleton();
+        Singleton st1 = Singleton.getSingleton();
         System.out.println(st1); // 317574433
         System.out.println(st1.hashCode());
-        Singleton st2  = Singleton.getSingleton();
-        System.out.println(st2); //317574433
+        Singleton st2 = Singleton.getSingleton();
+        System.out.println(st2); // 317574433
         System.out.println(st2.hashCode());
     }
 }
