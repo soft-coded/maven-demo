d04ea9af06891c165a98f93001d37a68db5c6e25
shrutanten <shrutanten.work@gmail.com>
2023-09-20 14:49:43 +0530

    Format the code better

--- src/main/java/org/example/FactoryDesignPattern.java
+++ src/main/java/org/example/FactoryDesignPattern.java
@@ -12,7 +12,7 @@ class Circle implements Shape {
     }
 }
 
-class Triangle implements  Shape {
+class Triangle implements Shape {
 
     @Override
     public void draw() {
@@ -30,65 +30,65 @@ class Cube implements Shape {
 
 class ShapeFactory {
     public Shape getShape(String shape) {
-        if(shape.equalsIgnoreCase("circle")) {
+        if (shape.equalsIgnoreCase("circle")) {
             return new Circle();
         }
-        if(shape.equalsIgnoreCase("triangle")) {
+        if (shape.equalsIgnoreCase("triangle")) {
             return new Triangle();
         }
-        if(shape.equalsIgnoreCase("cube")) {
+        if (shape.equalsIgnoreCase("cube")) {
             return new Cube();
         }
         return null;
     }
 }
+
 public class FactoryDesignPattern {
     public static void main(String[] args) {
-        ShapeFactory factory= new ShapeFactory();
+        ShapeFactory factory = new ShapeFactory();
 
-
-        Circle c = (Circle)factory.getShape("circle");
+        Circle c = (Circle) factory.getShape("circle");
         System.out.println(c);
 
-        Cube cb= (Cube)factory.getShape("cube");
+        Cube cb = (Cube) factory.getShape("cube");
         System.out.println(cb);
 
     }
 }
 
 /*
-class Employee {
-   int id;
-   String name;
-   Address address;
-   Login login;
-
-   Employee () {}
-   Employee (id, name, address, login) {
-       ...code to initialize
-   }
-}
-
-class Address {
-   String dNo;
-   String city;
-}
-
- class Login {
-    String email;
-    String password;
-    String role;
- }
-
- class Test {
-     public static void main(String[] args) {
-         Address address = new Address("102", "Chennai", "Tamilnadu" );
-         Login login = new Login("test@example.com", "abc.123", "admin");
-         Employee emp1 = new Employee(1001, "Raj", address, login );
-         (OR)
-         Employee emp2 =  factory.getEmployee();
-         emp2.setPassword("abc.456");
-     }
-
- }
-*/
+ * class Employee {
+ * int id;
+ * String name;
+ * Address address;
+ * Login login;
+ * 
+ * Employee () {}
+ * Employee (id, name, address, login) {
+ * ...code to initialize
+ * }
+ * }
+ * 
+ * class Address {
+ * String dNo;
+ * String city;
+ * }
+ * 
+ * class Login {
+ * String email;
+ * String password;
+ * String role;
+ * }
+ * 
+ * class Test {
+ * public static void main(String[] args) {
+ * Address address = new Address("102", "Chennai", "Tamilnadu" );
+ * Login login = new Login("test@example.com", "abc.123", "admin");
+ * Employee emp1 = new Employee(1001, "Raj", address, login );
+ * (OR)
+ * Employee emp2 = factory.getEmployee();
+ * emp2.setPassword("abc.456");
+ * }
+ * 
+ * }
+ */
