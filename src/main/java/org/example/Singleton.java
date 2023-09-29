b9c08517d3fac9f1f2119c84936b600d765e51e4
shrutanten <shrutanten.work@gmail.com>
2023-09-29 14:57:27 +0530

    Fix errors

--- src/main/java/org/example/Singleton.java
+++ src/main/java/org/example/Singleton.java
@@ -4,8 +4,8 @@ public class Singleton {
 
     private static Singleton singleton = new Singleton();
 
-    private Singleton() {
-    }
+    // private Singleton() {
+    // }
 
     public static Singleton getSingleton() {
         return singleton;
